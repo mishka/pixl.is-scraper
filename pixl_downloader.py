efd16e85e7b469a9ff6679f68829668aeffd7d92
浪人 <muhammedyuce@protonmail.ch>
2023-01-30 01:22:21 +0300

    Update pixl_downloader.py

--- pixl_downloader.py
+++ pixl_downloader.py
@@ -1,66 +1,58 @@
-from shutil import copyfileobj
+import shutil
 from datetime import datetime
-from os import getcwd
+import os
 
-from requests import get
+import requests
 from bs4 import BeautifulSoup
 from termcolor import colored
 
 
-class pixlr:
+class Pixlr:
     def __init__(self):
-        self.path = getcwd() + '/output/'
-        self.headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'}
-        self.link_to_posts = []
+        self.path = os.getcwd() + '/output/'
+        self.headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        }
+        self.post_links = []
         self.next_page = None
 
-
-    def log(self, text, colour):
+    def log(self, text, color):
         current_time = datetime.now().strftime('%H:%M:%S.%f')[:-3]
-        print(colored(f'| {current_time} |', 'white'),  colored(f'{text}', colour))
-
+        print(colored(f'| {current_time} |', 'white'), colored(f'{text}', color))
 
     def fetch_page(self, url):
-        return get(url, headers = self.headers).text
-
+        return requests.get(url, headers=self.headers).text
 
     def download_media(self, url, filename):
-        with open(self.path + filename, 'wb') as file:
-            req = get(url = url, headers= self.headers, stream = True)
-            req.raw.decode_content = True
-            copyfileobj(req.raw, file)
+        with open(os.path.join(self.path, filename), 'wb') as f:
+            res = requests.get(url, headers=self.headers, stream=True)
+            shutil.copyfileobj(res.raw, f)
 
-
-    def soup(self, content, album = False):
+    def parse_content(self, content, album=False):
         soup = BeautifulSoup(content, 'html.parser')
 
         if album:
             self.log('Parsing current album page..', 'yellow')
-            all_imgs = soup.find_all('div', attrs = {'class': 'list-item-image fixed-size'})
-
-            for entry in all_imgs:
-                self.link_to_posts.append(entry.find('a', attrs ={'class': 'image-container --media'})['href'])
-            
+            images = soup.find_all('div', class_='list-item-image fixed-size')
+            self.post_links.extend(
+                [img.find('a', class_='image-container --media')['href'] for img in images]
+            )
             try:
-                self.next_page = soup.find('a', attrs = {'data-pagination': 'next'})['href']
-            except KeyError:
+                self.next_page = soup.find('a', attrs={'data-pagination': 'next'})['href']
+            except TypeError:
                 self.next_page = None
         else:
             self.log('Parsing current picture page..', 'yellow')
-            return soup.find('input', attrs = {'id': 'embed-code-2', 'class': 'text-input'})['value']
-
+            return soup.find('input', id='embed-code-2', class_='text-input')['value']
 
     def process(self, url):
         self.log(f'Fetching: {url}', 'magenta')
-        self.soup(self.fetch_page(url), True)
+        self.parse_content(self.fetch_page(url), album=True)
 
         while self.next_page:
             self.log(f'Fetching: {self.next_page}', 'magenta')
-            self.soup(self.fetch_page(self.next_page), True)
+            self.parse_content(self.fetch_page(self.next_page), album=True)
 
-        for post in self.link_to_posts:
-            picture_url = self.soup(self.fetch_page(post))
-            filename = picture_url.split('i.pixl.is/')[1]
-            self.download_media(picture_url, filename)
-            self.log(f'Downloaded: {filename}', 'cyan')
+        for post in self.post_links:
+            picture_url = self.parse_content(self.fetch_page(post))
